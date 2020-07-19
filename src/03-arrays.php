543bfeac74b0f2549f6956029092bfd8d3de8939
Nikita Korniienko <n.kornienko.work@gmail.com>
2020-07-19 10:59:27 +0200

    Arrays homework is done

--- src/03-arrays.php
+++ src/03-arrays.php
@@ -8,8 +8,18 @@
  * @param  array  $input
  * @return array
  */
-function repeatArrayValues(array $input)
+function repeatArrayValues(array $input) : array
 {
+    $result = [];
+
+    foreach($input as $digit) {
+        
+        for ($i = 0; $i < $digit; $i++) {
+            array_push($result, $digit);
+        }
+    }
+
+    return $result;
 }
 
 /**
@@ -20,8 +30,36 @@ function repeatArrayValues(array $input)
  * @param  array  $input
  * @return int
  */
-function getUniqueValue(array $input)
+function getUniqueValue(array $input) : int
 {
+    //ATTENTION: a lot of offsets here, be careful ;)
+
+    if ( empty($input) ) {
+        return 0;
+    }
+
+    //sort in order to avoid full array scan
+    sort($input, SORT_NUMERIC);
+    
+    //check if first elem is uniq
+    if ($input[0] !== $input[1]) {
+        return $input[0];
+    }
+
+    for ($i = 1, $count = count($input) - 1; $i < $count; $i++) {
+        
+        if ( $input[$i] !== $input[$i - 1] && $input[$i] !== $input[$i + 1]) {
+            
+            return $input[$i];
+        } 
+    }
+    
+    //check if last elem is uniq 
+    if ( $input[count($input) - 1] !== $input[count($input) - 2] ) {
+        return $input[count($input) - 1];
+    }
+    
+    return 0;
 }
 
 /**
@@ -48,6 +86,25 @@ function getUniqueValue(array $input)
  * @param  array  $input
  * @return array
  */
-function groupByTag(array $input)
+function groupByTag(array $input) : array
 {
+    $result = [];
+
+    foreach ($input as $name) {
+        
+        foreach ($name['tags'] as $tag) {
+            
+            if ( !array_key_exists($tag, $result)) {
+                $result[$tag] = [];
+            }
+            
+            array_push($result[$tag], $name['name']);   
+        }
+    }
+
+    foreach ($result as $tag => $namesArr) {
+        sort($result[$tag], SORT_STRING);
+    }
+
+    return $result;
 }
